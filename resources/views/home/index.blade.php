0d6ffde792d60f23283214b6343fe359579590a7
bon_92 <mirai1735@icloud.com>
2021-03-02 23:41:24 +0900

    first comimit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+@section('content')
+
+<div class="card mt-5">
+  <div class="card-body">
+    <p><a href="" class="btn btn-primary">画像一覧</a></p>
+  </div>
+</div>
+
+@endsection
---
+++ resources/views/photo/list.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.layout')
+@section('content')
+
+<div class="row">
+  <div class="col-lg-4">
+    画像です
+  </div>
+  <div class="col-lg-4">
+    画像です
+  </div>
+  <div class="col-lg-4">
+    画像です
+  </div>
+</div>
+
+@endsection
